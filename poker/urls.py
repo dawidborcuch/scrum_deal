a951f2f16c72b04cba4390709440b3ebb02e701d
DawidBo <dawid.borcuch@gmail.com>
2025-07-21 21:58:49 +0200

    poker

--- poker/urls.py
+++ poker/urls.py
@@ -5,6 +5,7 @@ app_name = 'poker'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('join_table/', views.join_table, name='join_table'),
     path('table/<str:table_name>/', views.table_view, name='table'),
     path('check_croupier/<str:table_name>/', views.check_croupier, name='check_croupier'),
 ] 
--- poker/views.py
+++ poker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.core.cache import cache
+from django.urls import reverse
 
 # Create your views here.
 
@@ -16,13 +17,26 @@ class TableView(TemplateView):
         context['table_name'] = kwargs.get('table_name')
         return context
 
+def join_table(request):
+    if request.method == 'POST':
+        table_name = request.POST.get('table_name')
+        nickname = request.POST.get('nickname')
+        role = request.POST.get('role', 'participant')
+        is_croupier = request.POST.get('is_croupier') == 'on'
+        if not table_name or not nickname:
+            return redirect('poker:home')
+        request.session['nickname'] = nickname
+        request.session['role'] = role
+        request.session['is_croupier'] = is_croupier
+        return redirect(reverse('poker:table', args=[table_name]))
+    return redirect('poker:home')
+
 def table_view(request, table_name):
-    nickname = request.GET.get('nickname', '')
-    role = request.GET.get('role', 'participant')
-    is_croupier = request.GET.get('is_croupier', '0') == '1'
+    nickname = request.session.get('nickname', '')
+    role = request.session.get('role', 'participant')
+    is_croupier = request.session.get('is_croupier', False)
     if not nickname:
         return redirect('poker:home')
-    
     return render(request, 'poker/table.html', {
         'table_name': table_name,
         'nickname': nickname,
