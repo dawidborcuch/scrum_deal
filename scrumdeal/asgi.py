b2b9bd8072738d2582dbd62c15b799b33fc57eb4
DawidBo <dawid.borcuch@gmail.com>
2025-07-21 21:53:16 +0200

    poker

--- scrumdeal/asgi.py
+++ scrumdeal/asgi.py
@@ -1,31 +1,12 @@
-"""
-ASGI config for scrumdeal project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrumdeal.settings')
 
-django_asgi_app = get_asgi_application()
-
-import django
-django.setup()
-
-from poker.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        __import__('poker.routing').routing.websocket_urlpatterns
     ),
-})
+}) 
