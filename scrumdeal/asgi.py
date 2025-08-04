1737542b8786c6f643db8311e30730d212229859
DawidBo <dawid.borcuch@gmail.com>
2025-08-04 17:10:33 +0200

    Naprawiono import routing w ASGI - przeniesiono import po załadowaniu Django

--- scrumdeal/asgi.py
+++ scrumdeal/asgi.py
@@ -3,9 +3,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrumdeal.settings')
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from poker.routing import websocket_urlpatterns
+
+# Importujemy routing po załadowaniu Django
+def get_websocket_urlpatterns():
+    from poker.routing import websocket_urlpatterns
+    return websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "websocket": URLRouter(get_websocket_urlpatterns()),
 }) 
